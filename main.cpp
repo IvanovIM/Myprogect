6e6923da3b0a0ede381417e0480b4ec46ccd7432
lab01 (IvanovIM) <IvanovIvM@mpei.ru>
2024-03-14 22:21:39 +0300

    code: Разбиение исходного кода на функции

--- main.cpp
+++ main.cpp
@@ -3,42 +3,44 @@
 
 using namespace std;
 
-int main(){
+ struct Input {
+        vector<double> numbers;
+        size_t bin_count{};
+        size_t number_count;
+
+};
+
+Input
+input_data(){
 
     setlocale(LC_ALL, "RUSSIAN");
 
-    const size_t SCREEN_WIDTH = 80;
-    const size_t MAX_ASTERISK = SCREEN_WIDTH - 3 - 1;
-
-
-    size_t number_count; //количество чисел в массиве
-
+    size_t number_count;
     cerr << "¬ведите количество чисел: ";
     cin >> number_count;
 
+    Input in;
 
-    vector<double> numbers(number_count); //вектор из этих чисел
+    in.numbers.resize(number_count);
 
     cerr << "¬ведите через пробел элементы массива: ";
 
-    for (double i = 0; i < number_count; i++){
-        cin >> numbers[i];
+    vector<double> numbers(number_count);
+    for (size_t i = 0; i < number_count; i++) {
+        cin >> in.numbers[i];
     }
 
-    //////////////////////////////////
+    size_t bin_count;
+    cerr << "¬ведите количество корзин: ";
+    cin >> in.bin_count;
 
-    size_t bin_count; // количество корзин
+    return in;
+};
 
-    cerr << " ¬ведите количество корзин:";
-    cin >> bin_count;
-    cerr << endl;
-
-
-    vector<size_t> bins(bin_count); // массив из кол-ва элементов в столбцах
-
-
-    double min = numbers[0];
-    double max = numbers[0];
+void
+find_minmax(const vector<double>& numbers, double& min, double& max) {
+    min = numbers[0];
+    max = numbers[0];
 
     for (double x : numbers) {
         if (x < min) {
@@ -48,10 +50,18 @@ int main(){
             max = x;
         }
     }
+}
 
-    double bin_size = (max - min) / bin_count; // размер корзины
+auto
+make_histogram(const vector<double> numbers, size_t bin_count){
 
-    for (size_t i = 0; i < number_count; i++) {
+    vector <size_t> bins(bin_count);
+    double min, max;
+    find_minmax(numbers, min, max);
+
+    double bin_size = (max - min) / bin_count;
+
+    for (size_t i = 0; i < numbers.size(); i++) {
         bool found = false;
         for (size_t j = 0; (j < bin_count - 1) && !found; j++) {
             auto lo = min + j * bin_size;
@@ -67,50 +77,32 @@ int main(){
         }
     }
 
+    return bins;
+}
 
-
-
-    size_t max_count = bins[0]; // количество элементов в большем столбце
-    int k = 0;
-    for (auto i = 0; i < bin_count; i++){
-       if (bins[i] >= max_count){
-            max_count = bins[i];
-            k=i;
-       }
-    }
-
-    float procent;
+void
+show_histogram_text(const vector<size_t> bins, size_t number_count, size_t bin_count){
 
     for (size_t i = 0; i < bin_count; i++){
 
-        int procent =((float)bins[i] / number_count)*100;
-
         if (bins[i] < 100) cout << " ";
         if (bins[i] < 10) cout << " ";
-        cout << procent << "%|";
+        cout << bins[i] << "|";
 
-        size_t height = 0;
-
-
-        if (bins[i] < max_count){//маштабирование
-            size_t height = 76 * (static_cast<double>(bins[i]) / max_count);
-
-            for(size_t j=0; j < height; j++){
+        for(size_t j=0; j < bins[i]; j++){
                     cout << "*";
-            }
-        }
-
-        if(i == k)
-        {
-            for(size_t j=0; j < (76*1.0); j++)
-            {
-
-                cout<<"*";
-            }
         }
          cout<<endl;
     }
+}
 
+
+int
+main(){
+
+    auto in = input_data();
+    auto bins = make_histogram(in.numbers, in.bin_count);
+    show_histogram_text(bins, in.number_count, in.bin_count);
 
     return 0;
 }

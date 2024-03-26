3968685cce1cc22823f6b5009cf5160fd3fbf563
lab01 (IvanovIM) <IvanovIvM@mpei.ru>
2024-03-26 20:38:47 +0300

    code: Обработка ошибок

--- main.cpp
+++ main.cpp
@@ -50,11 +50,17 @@ main(int argc, char* argv[]){
     CURL* curl = curl_easy_init();
 
     if (argc > 1){
+
         CURLcode res;
         curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
         res = curl_easy_perform(curl);
+
+        if(res != CURLE_OK)
+            fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+
         curl_easy_cleanup(curl);
-        return 0;
+
+        exit(1);
     }
 
 

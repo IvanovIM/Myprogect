8273ec316f283612754a76f602905d2c2407eca7
lab01 (IvanovIM) <IvanovIvM@mpei.ru>
2024-03-30 20:36:31 +0300

    files: Последние версии файлов

--- main.cpp
+++ main.cpp
@@ -4,6 +4,9 @@
 #include "histogram.h"
 #include "text.h"
 #include "svg.h"
+#include <sstream>
+#include <string>
+
 
 
 using namespace std;
@@ -16,13 +19,13 @@ using namespace std;
 };
 
 Input
-input_data(istream& in){  //& тк нельзя скопировать поток ввода   istream управляет извлечением объектов
+input_data(istream& in, bool prompt){  //& тк нельзя скопировать поток ввода   istream управляет извлечением объектов
 
     setlocale(LC_ALL, "RUSSIAN");
 
     Input in2;
 
-    bool prompt = true;
+    //bool prompt = true;
     if (prompt) cerr << "Введите количество чисел: ";
     in >> in2.number_count;
 
@@ -43,34 +46,71 @@ input_data(istream& in){  //& тк нельзя скопировать поток ввода   istream управл
     return in2;
 };
 
+size_t
+write_data(void* items, size_t item_size, size_t item_count, void* ctx) {
 
-int
-main(int argc, char* argv[]){
+    size_t data_size = item_size * item_count;
+
+    stringstream* buffer = reinterpret_cast<stringstream*>(ctx);
+    buffer->write(reinterpret_cast<const char*>(items), data_size);
+
+    return data_size;
+}
+
+
+Input
+download(const string& address) {
+    stringstream buffer;
 
     CURL* curl = curl_easy_init();
 
-    if (argc > 1){
-
+    if(curl) {
         CURLcode res;
-        curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
+        curl_easy_setopt(curl, CURLOPT_URL, address.c_str());
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buffer);
+
         res = curl_easy_perform(curl);
 
-        if(res != CURLE_OK)
+        if(!res){
+            curl_off_t speed;
+            res = curl_easy_getinfo(curl, CURLINFO_SPEED_DOWNLOAD_T, &speed);
+            if(!res){
+                cerr << "Download speed " << speed << endl;
+            }
+        }
+
+        if(res != CURLE_OK){
             fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
-
+            exit(1);
+        }
         curl_easy_cleanup(curl);
-
-        exit(1);
     }
 
+    return input_data(buffer, false);
+}
 
-    curl_global_init(CURL_GLOBAL_ALL);
-    auto in = input_data(cin);
-    auto bins = make_histogram(in.numbers, in.bin_count);
-    show_histogram_text(bins, in.number_count, in.bin_count);
 
+int
+main(int argc, char* argv[]) {
+
+    Input input;
+
+    if (argc > 1) {
+        input = download(argv[1]);
+    } else {
+        input = input_data(cin, true);
+    }
+
+    const auto bins = make_histogram(input.numbers, input.bin_count);
+    show_histogram_text(bins, input.number_count, input.bin_count);
     return 0;
 }
 
+
+    //curl_global_init(CURL_GLOBAL_ALL);
+    //auto in = input_data(cin);
+    //auto bins = make_histogram(in.numbers, in.bin_count);
+    //show_histogram_text(bins, in.number_count, in.bin_count);
 
 // cin - переменная которая представляет поток
